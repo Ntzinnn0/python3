3796d8a54f516259df2173d96ff7175afe0126c9
Nathan Barbosa <nathabarbosa517@gmail.com>
2025-03-09 19:34:14 -0300

    09/03

---
+++ python3/aula06.py
@@ -0,0 +1,14 @@
+#exercício com variáveis e tipos de dados
+nome = "Erik"
+sobrenome = "Natanael"
+idade = 19
+ano_nascimento = 2025 - idade 
+maior_de_idade = idade >= 18
+altura_metros = float(1.84)
+
+print('Nome:', nome)
+print('Sobrenome', sobrenome)
+print('Idade:',idade)
+print('Ano de nascimento', ano_nascimento)
+print('E maior de idade?', maior_de_idade)
+print('altura em metros:', altura_metros)
