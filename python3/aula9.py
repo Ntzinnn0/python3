3e10bf4082f65fb9d1d215cc11eda94d787e4d5f
Nathan Barbosa <nathabarbosa517@gmail.com>
2025-03-13 23:37:55 -0300

    13/03

---
+++ python3/aula9.py
@@ -0,0 +1,7 @@
+#  Exercício de programação - Cálculo do IMC (Índice de Massa Corpórea)
+nome = 'Erik Natanael'
+altura = 1.84
+peso = 79
+imc = peso / (altura * altura)
+
+print('{} tem: {} de altura e seu imc e: {:.2f}'.format(nome, altura, imc))
